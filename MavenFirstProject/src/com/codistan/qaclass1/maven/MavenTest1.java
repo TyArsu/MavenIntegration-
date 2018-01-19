5d39249d5f3337224bbb6b65f52a0209849dab90
kasia <kasia@kasias-air.att.net>
2018-01-19 17:16:58 -0600

    MavenFirstTest Automation

---
+++ MavenFirstProject/src/com/codistan/qaclass1/maven/MavenTest1.java
@@ -0,0 +1,21 @@
+package com.codistan.qaclass1.maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MavenTest1 {
+	
+	@Test
+
+	public void Test1() {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/kasia/Documents/Libraries/drivers/chromedriver3");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com");
+		driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("Fidget"+Keys.ENTER);
+		driver.quit();
+		
+	}
+}
---
+++ MavenFirstProject/src/com/codistan/qaclass1/maven/MavenTest2.java
@@ -0,0 +1,17 @@
+package com.codistan.qaclass1.maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenTest2 {
+
+	public void Test1() {
+		System.setProperty("webdriver.chrome.driver", "/Users/kasia/Documents/Libraries/drivers/chromedriver3");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com");
+		driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("Fidget"+Keys.ENTER);
+		driver.quit();
+		
+	}
+}
